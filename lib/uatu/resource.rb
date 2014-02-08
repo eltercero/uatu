ec3b6415b508383401f336bc71388b93f8b7eb4d
Victor <ilovemayalopez@gmail.com>
2014-02-09 00:11:05 +0100

    I forgot I was already using ActiveSupport...

--- lib/uatu/resource.rb
+++ lib/uatu/resource.rb
@@ -14,7 +14,7 @@ module Uatu
     def underscore_keys(hash)
       _hash = {}
       hash.each do |k, v|
-        _hash[underscore(k)] = if v.is_a?(Hash)
+        _hash[k.to_s.underscore] = if v.is_a?(Hash)
           underscore_keys(v)
         elsif v.is_a?(Array) and v.first.is_a?(Hash)
           v.map{|h| underscore_keys(h)}
@@ -36,15 +36,6 @@ module Uatu
       end
       _args
     end
-
-    # Ripoff from Rails ActiveSupport
-    def underscore(camel_cased_word)
-      camel_cased_word.to_s.gsub(/::/, '/').
-        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-        gsub(/([a-z\d])([A-Z])/,'\1_\2').
-        tr("-", "_").
-        downcase
-   end
 
   end
 
